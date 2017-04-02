19d09c9ed950bc7150f8a1cfa9aaa40c67290805
Jari <Sadye@users.noreply.github.com>
2017-04-02 20:49:16 +0200

    Total rewrite. Feel free to test.

--- script.js
+++ script.js
@@ -1,150 +1,149 @@
-var drawingData = {
-	startX: 0,
-	startY: 0,
-	colors: [["rood", "blauw", "wit", "niets"]],
-	kill: false
-};
-
 var modhash = window.reddit.modhash;
-var timeSinceLastRetrieval = 5000;
-var index = 0;
-var sec = 0;
 var currentVersion = 7;
-
-const colorScheme = {
-	"wit": 0,
-	"lgrijs": 1,
-	"dgrijs": 2,
-	"zwart": 3,
-	"roze": 4,
-	"rood": 5,
-	"oranje": 6,
-	"bruin": 7,
-	"geel": 8,
-	"lgroen": 9,
-	"groen": 10,
-	"lblauw": 11,
-	"blauw": 12,
-	"dblauw": 13,
-	"magenta": 14,
-	"paars": 15,
-	"niets": -1,
+var drawingData = {
+	startX:50,
+	startY:0,
+	kill:false,
+	colors:[[5,5,5],[5,5,5]],
+	newVersion: -1
 };
 
-function replaceTextWithNumbers(){
-	for (var i = 0; i < drawingData.colors[0].length * drawingData.colors.length; i++) {
-		var width = drawingData.colors[0].length;
-		var height = drawingData.colors.length;
-		var tempx = i % width;
-		var tempy = Math.floor(i / width);
-		console.log(width, height, tempx, tempy, drawingData.colors);
-		for (var key in colorScheme) {
-			if (drawingData.colors[tempy][tempx] == key) {
-				drawingData.colors[tempy][tempx] = colorScheme[key];
-				break;
-			}
-		}
-	}
+var intervalId = -1;
+var secondsLeft = -1;
+
+var currentX = 0;
+var currentY = 0;
+
+function start(){
+	updateGoal();
+	setInterval(updateGoal(), 300 * 1e3);
+	setTimeout(checkPixels(), 2000);
 }
 
-function retrieveAndDraw(doDraw) {
-
-		// retrieve data
-		const url = 'https://raw.githubusercontent.com/Sadye/rPlace/master/data.json' + '?no-cache=' + (new Date).getTime();
-
-		//TODO implement random selection of multiple files
-		//better to be handled @ serer
-
+// retrieves target from server
+function updateGoal() {
+	const url = 'https://raw.githubusercontent.com/Sadye/rPlace/master/data.json' + '?no-cache=' + (new Date).getTime();
+	//TODO implement random selection of multiple files
+	//better to be handled @ serer
 		fetch(url)
-		.then((resp) => resp.json())
-		.then(function(data) {
-			drawingData.startX = data.startX;
-			drawingData.startY = data.startY;
-			drawingData.colors = data.colors;
-
-			if (currentVersion < data.newVersion) {
-				document.body.innerHTML = '<center><br><br><br><br><br><br><h1 style="font-size: 20pt;">Je script is verouderd! Download alsjeblieft de nieuwe update (v' + data.newVersion + '). <br><br><br><br> <a href=https://raw.githubusercontent.com/Sadye/rPlace/master/script.js>Script</a> | <a target="_blank" href="https://discord.gg/EU4NhBn">Discord</a> | <a target="_blank" href="https://github.com/Sadye/rPlace">Github</a></h1></center>';
-				alert('Nieuwe update beschikbaar!');
-			}
-
-			index = Math.floor(Math.random() * (drawingData.colors[0].length * drawingData.colors.length));
-			timeSinceLastRetrieval = 0;
-			replaceTextWithNumbers();
-
-			if (doDraw) draw(0);
-
-		})
-		.catch(function(error) {
-			console.log(error);
-			setTimeout(() => retrieveAndDraw(doDraw), 10 * 1e3);
-		});
+	.then((resp) => resp.json())
+	.then(function(data) {
+		// update object that holds drawing data
+		drawingData.startX = data.startX;
+		drawingData.startY = data.startY;
+		drawingData.colors = data.colors;
+		if (currentVersion < data.newVersion) {
+			// script needs updating
+			document.body.innerHTML = '<center><br><br><h1>Je script is verouderd! Download alsjeblieft de nieuwe update (v' + data.newVersion + '). <br><br><br><br> <a href=https://raw.githubusercontent.com/Sadye/rPlace/master/script.js>Script</a> | <a href="https://discord.gg/EU4NhBn">Discord</a></h1></center>';
+			return;
+		}
+		if (drawingData.kill && !data.kill) {
+			// script was restarted after kill command
+			setTimeout(checkPixels(), 2000);
+		}
+		// pick a random x and y out of the drawing data
+		currentY = Math.floor(Math.random() * drawingData.colors.length);
+		currentX = Math.floor(Math.random() * drawingData.colors[currentY].length);
+	})
+	.catch(function(error) {
+		console.log(error);
+		console.log("Retrying: ");
+		setTimeout(() => updateGoal(), 5000);
+	});
 }
 
-function draw(seconds) {
+function checkPixels() {
+	// killswitch
 	if (drawingData.kill) {
-		//setTimeout(() => retrieveAndDraw(true), 250 * 1e3);
-		// it's automatically updated every 250 s
 		return;
 	}
-	//retrieveAndDraw(false);
-	var width = drawingData.colors[0].length;
-	var height = drawingData.colors.length;
-	index++;
-	console.log(width, height, index);
-    index = index % (width * height);
-    sec = seconds = Math.ceil(seconds);
-    setTimeout(() => {
-        const x = index % width;
-        const y = Math.floor(index / width);
-        // vanaf nu kan een flagcolor -1 zijn, dan wordt die kleur als altijd correct gezien
+	// remove info message interval
+	window.clearInterval(intervalId);
+	setTimeout( () => {
+		// after a second, check a new pixel
+		// automatically wrap around to the next line, or back to the start
+		// if we go out of bounds
+		if (currentX > drawingData.colors[currentY].length - 1) {
+			currentY += 1;
+			currentX = 0;
+		}
+		if (currentY > drawingData.colors.length - 1) {
+			currentY = 0;
+		}
+		// ignore transparant pixels
+		if (drawingData.colors[currentY][currentX] == -1) {
+			currentX++;
+			return checkPixels();
+		}
+		var ax = currentX + drawingData.startX;
+		var ay = currentY + drawingData.startY;
+		console.log("Checking pixel at ("+ ax + ", " + ay +"). It should be color: " + drawingData.colors[currentY][currentX]);
 
-        const flagColor = drawingData.colors[y][x];
-        if (flagColor == -1) {
-        	return draw(0);
-        }
-        // const xChange = flagColor != drawingData.colors[y][x - 1] || flagColor != drawingData.colors[y][x + 1];
-        // const yChange = (drawingData.colors[y - 1] && flagColor != drawingData.colors[y - 1][x]) || (drawingData.colors[y + 1] && flagColor != drawingData.colors[y + 1][x]);
-        // if  (!xChange && !yChange) {
-        //     return draw(0);
-        // }
-        const ax = x + drawingData.startX;
-        const ay = y + drawingData.startY;
-
-        $.get("https://www.reddit.com/api/place/pixel.json?x=" + ax + "&y=" + ay)
-        .then(res => {
-        	console.log(res);
-            if (res.color == flagColor) {
-                console.log((ax + ", " + ay) + " worden overgeslagen omdat ze al kloppen!");
-                return draw(1);
-            }
-            var color;
-            for (var key in colorScheme) {
-            	if (colorScheme[key] == flagColor) {
-            		color = key;
-            		break;
-            	}
-            }
-            console.log("Pixel tekenen op locatie " + ax + ", " + ay + " ("+color+") (https://www.reddit.com/r/place/#x=" + ax + "&y=" + ay + ")");
-            $.ajax({ url: "https://www.reddit.com/api/place/draw.json", type: "POST",
-                headers: { "x-modhash": modhash }, data: { x: ax, y: ay, color: flagColor }
-            })
-            .done(data => {
-            	//data=json.parse(data);
-            	draw(data.wait_seconds);
-            	console.log(data);
-            	return;
-            })
-            .error(data => {
-            	//data = JSON.parse(data);
-            	draw(data.responseJSON.wait_seconds);
-            	return;
-            });
-        });
-    }, seconds * 1000);
+		// check for the correct pixels
+		$.get("https://www.reddit.com/api/place/pixel.json?x=" + ax + "&y=" + ay)
+	    .then(res => {
+	    	if (res.color == drawingData.colors[currentY][currentX]) {
+	    		// color correct, so check the next pixel
+	    		currentX++;
+	    		return checkPixels();
+	    	} else {
+	    		// color incorrect, so overwrite!
+	    		return drawPixel();
+	    	}
+	    }).fail(res => {
+	    	// some error, try again in a second
+	    	return checkPixels();
+	    })
+	}, 1000);
 }
 
-replaceTextWithNumbers();
-retrieveAndDraw(true);
-window.setInterval( () => console.log("Pixel tekenen over " + (sec) + " seconden. Data ophalen " + (timeSinceLastRetrieval > 250 ? "na volgende pixel. " : "over " + (250-timeSinceLastRetrieval) + " seconden!")), 10 * 1e3);
-window.setInterval(() => {sec--;timeSinceLastRetrieval++}, 1e3);
-window.setInterval(() => retrieveAndDraw(false), 250 * 1e3);
+// draws a pixel
+function drawPixel() {
+	setTimeout( () => {
+		// calculate correct x, y, color
+		var ax = currentX + drawingData.startX;
+		var ay = currentY + drawingData.startY;
+		var newColor = drawingData.colors[currentY][currentX];
+		// try to draw
+		console.log("Pixel tekenen op locatie (" + ax + ", " + ay + ") Kleur: "+newColor+" (https://www.reddit.com/r/place/#x=" + ax + "&y=" + ay + ")");
+		$.ajax({ url: "https://www.reddit.com/api/place/draw.json", type: "POST",
+            headers: { "x-modhash": modhash }, data: { x: ax, y: ay, color: newColor }
+        })
+        .done( res => {
+        	// drawing was succesfull
+        	// so try again after cooldown
+        	setTimeout(() => {
+        		checkPixels()
+        	}, res.wait_seconds * 1e3)
+        	console.log("Succes! Nieuwe poging over " + res.wait_seconds + " seconden.");
+
+        	// and show an info message so people know it's still working
+        	secondsLeft = res.wait_seconds;
+        	intervalId = setInterval( () => {
+        		secondsLeft -= 10;
+        		console.log("Nog " + secondsLeft + " seconden tot de volgende actie!");
+        	}, 10 * 1e3)
+        	return;
+        })
+        .error( res => {
+        	// error, cooldown not passed probably
+        	// give info message. If we received a cooldown error (status 429)
+        	// use that value as the next action, else try again in ten seconds
+        	setTimeout(() => {
+        		checkPixels()
+        	}, Math.max(Math.ceil(res.responseJSON.wait_seconds), 10) * 1e3);
+        	console.log("Probleem! Nieuwe poging over " + Math.max(Math.ceil(res.responseJSON.wait_seconds), 10) + " seconden.");
+        	
+        	// and some info logging to the user
+        	secondsLeft = Math.ceil(res.responseJSON.wait_seconds)
+        	intervalId = setInterval( () => {
+        		secondsLeft -= 10;
+        		console.log("Nog " + secondsLeft + " seconden tot de volgende actie!");
+        	}, 10 * 1e3)
+        	return;
+        });
+
+	}, 500)
+}
+
+start();
