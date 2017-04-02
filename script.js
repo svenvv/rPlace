1b4b25b7a166992b7c9dba1142ce08f50dae6ec3
Jari <Sadye@users.noreply.github.com>
2017-04-02 15:56:35 +0200

    New C&C URL

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function retrieveAndDraw(doDraw) {
 
 	if (timeSinceLastRetrieval > 360) {
 		// retrieve data
-		const url = 'https://www.basvdwollenberg.nl/place.json';
+		const url = 'https://placenl.basvdwollenberg.nl/data.json';
 
 		//TODO implement random selection of multiple files
 		//better to be handled @ serer
